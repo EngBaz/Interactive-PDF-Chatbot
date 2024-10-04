73d2c1e942a0454c12f204a3c612d2735d5701eb
Roland Baz <94540491+EngBaz@users.noreply.github.com>
2024-10-04 14:36:58 +0200

    update

--- main.py
+++ main.py
@@ -1,122 +1,283 @@
 import os
-import streamlit as st
-import pandas as pd
 
-from langchain_openai import ChatOpenAI
 from rag_utils import *
-from PyPDF2 import PdfReader
+
 from langchain_groq import ChatGroq
+from pydantic import (
+    BaseModel,
+    Field,
+)
+from langchain_core.prompts import ChatPromptTemplate
+from typing_extensions import TypedDict
+from typing import (
+    List,
+)
+from langgraph.graph import END, StateGraph
+from langchain.schema import Document
+from langchain_community.tools.tavily_search import TavilySearchResults
 
 from dotenv import load_dotenv
 load_dotenv()
 
-COHERE_API_KEY = os.environ["COHERE_API_KEY"]
-GROQ_API_KEY = os.environ["GROQ_API_KEY"]
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-    
+# Set up streamlit UI
 st.set_page_config(
-    page_title="Chatbot Assistant",
+    page_title="Conversational Agent",
     page_icon="🤖",
     layout="wide",
     initial_sidebar_state="expanded",
     )
 
-def main():
-    
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-    
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-    
-    with st.sidebar:
-            
-        st.header("SETUP")
-            
-        file_format = st.selectbox(label="Select file format:", options=["...", "pdf", "txt", "csv"])
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-        uploaded_file = st.file_uploader("Upload a file:", type=[file_format])
-            
-        st.header("PARAMETERS")
-                
-        max_new_tokens = st.number_input("Select a max token value:", min_value=1, max_value=8000, value=1000)
-                
-        temperature = st.number_input("Select a temperature value:", min_value=0.0, max_value=1.0, value=0.00)
-         
-    llm = ChatGroq(model="llama-3.1-70b-versatile", temperature=temperature, max_tokens=max_new_tokens)
-        
-    try:
-        if not uploaded_file:
-            
-            welcome_message()       
-            st.stop()
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-        elif uploaded_file.size == 0:
-             
-             
-            st.info("The uploaded file is empty. Please upload a valid document.")
-            st.stop()
-        
-        elif uploaded_file is not None:
-                
-            if file_format == "pdf":
-                    
-                pdf_reader = PdfReader(uploaded_file)
-                data = "".join(page.extract_text() for page in pdf_reader.pages)
-                    
-            elif file_format == "txt":
-            
-                data = uploaded_file.read().decode("utf-8")
-                
-            elif file_format == "csv":
-                    
-                data = pd.read_csv(uploaded_file)
-                data = data.to_string(index=False)
-                    
-            else:
-                    
-                st.warning("Unsupported file format. Please upload a valid document (PDF, TXT, or CSV).")
-            
+# API keys
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+COHERE_API_KEY = os.getenv("COHERE_API_KEY")
+
+# Initiate the sidebar of the streamlit application
+with st.sidebar:
+    st.header("Setup")
+
+    file_format = st.selectbox(label="Select file format:", options=["...", "pdf", "txt", "csv"])
+
+    uploaded_file = st.file_uploader("Upload a file:", type=[file_format])
+
+    st.header("Model Settings")
+
+    max_new_tokens = st.number_input("Select a max token value:", min_value=1, max_value=8000, value=1000)
+
+    temperature = st.number_input("Select a temperature value:", min_value=0.0, max_value=2.0, value=0.00)
+
+# Load the LLM from GROQ
+llm = ChatGroq(model="llama-3.1-70b-versatile", temperature=temperature, max_tokens=max_new_tokens)
+
+# Initiate Tavily web search tool
+web_search_tool = TavilySearchResults(k=5)
+
+# Load the retriever and the RAG chain
+retriever = retrieve_documents(uploaded_file, file_format)
+rag_chain = create_rag_chain(retriever, llm)
+
+
+class GradeDocuments(BaseModel):
+    """
+    Binary score for relevance check on retrieved documents.
+    """
+
+    binary_score: str = Field(description="Documents are relevant to the question, 'yes' or 'no'")
+
+
+structured_llm_doc_grader = llm.with_structured_output(GradeDocuments)
+
+grader_system = """You are a grader assessing relevance of a retrieved document to a user question.
+If the document contains keyword(s) or semantic meaning related to the question, grade it as relevant.
+Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
+
+grader_human = "Here is the retrieved document: \n\n {document} \n\n Here is the user question: \n\n {question}"
+
+grade_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", grader_system),
+        ("human", grader_human)
+    ]
+)
+
+retrieval_grader = grade_prompt | structured_llm_doc_grader
+
+
+def web_search(state):
+    """
+    Web search based on the question
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Appended web results to documents
+    """
+
+    print("---WEB SEARCH---")
+    question = state["question"]
+    documents = state["documents"]
+
+    docs = web_search_tool.invoke({"query": question})
+    web_results = "\n".join([d["content"] for d in docs])
+    web_results = Document(page_content=web_results)
+    if documents is not None:
+        documents.append(web_results)
+    else:
+        documents = [web_results]
+
+    return {"documents": documents, "question": question}
+
+
+def decide_to_generate(state):
+    """
+    Determines whether to generate an answer, or add web search
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Binary decision for next node to call
+    """
+
+    print("---ASSESS GRADED DOCUMENTS---")
+
+    web_search_flag = state["web_search"]
+
+    if web_search_flag == "Yes":
+
+        print("---DECISION: NOT ALL DOCUMENTS ARE RELEVANT TO QUESTION, INCLUDE WEB SEARCH---")
+        return "websearch"
+    else:
+
+        print("---DECISION: GENERATE---")
+        return "generate"
+
+
+def retrieve(state):
+    """
+    Retrieve documents from vectorstore
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, documents, that contains retrieved documents
+    """
+    print("---RETRIEVE---")
+    question = state["question"]
+
+    documents = retriever.invoke(question)
+    return {"documents": documents, "question": question}
+
+
+def generate(state):
+    """
+    Generate answer using RAG on retrieved documents
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, generation, that contains LLM generation
+    """
+    print("---GENERATE---")
+    question = state["question"]
+    documents = state["documents"]
+
+    generation = rag_chain.invoke(
+        {"input": question},
+    )["answer"]
+
+    return {"documents": documents, "question": question, "generation": generation}
+
+
+def grade_documents(state):
+    """
+    Determines whether the retrieved documents are relevant to the question
+    If any document is not relevant, we will set a flag to run web search
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Filtered out irrelevant documents and updated web_search state
+    """
+
+    print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
+    question = state["question"]
+    documents = state["documents"]
+
+    filtered_docs = []
+    web_search_flag = "No"
+
+    for doc in documents:
+        score = retrieval_grader.invoke({"question": question, "document": doc.page_content})
+        grade = score.binary_score
+
+        if grade.lower() == "yes":
+            print("---GRADE: DOCUMENT RELEVANT---")
+            filtered_docs.append(doc)
+
         else:
-            
-            pass
-            
-    except Exception as e:
-            
-        st.info(f"An error occurred the upload: {str(e)}")
+            print("---GRADE: DOCUMENT NOT RELEVANT---")
+            web_search_flag = "Yes"
+            continue
+    return {"documents": filtered_docs, "web_search": web_search_flag}
 
-    if 'data' in locals():
-            
-        retriever = configure_hybrid_search(data)
 
-        conversational_rag_chain = configure_rag_chain(retriever, llm)
-     
-    if prompt := st.chat_input("Ask a question"):
-        
-        st.session_state.messages.append({"role": "user", "content": prompt})
-            
-        with st.chat_message("user"): 
-                
-            st.write_stream(stream_data(prompt))    
-            
-        with st.chat_message("assistant"):
-                
-            response = conversational_rag_chain.invoke(
-                {"input": prompt},
-                config={"configurable": {"session_id": "session1"}},
-                )["answer"]
-                
-            st.write_stream(stream_data(response))
-                
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
 
-if __name__ == "__main__":
-    
-    main()
-    
-        
-    
-    
-        
+    Attributes:
+        question: question
+        generation: LLM generation
+        web_search: whether to add search
+        documents: list of documents
+
+    """
+    question: str
+    generation: str
+    web_search: str
+    documents: List[str]
+
+
+# Build the graph
+workflow = StateGraph(GraphState)
+
+# Define the nodes
+workflow.add_node("retrieve", retrieve)
+workflow.add_node("grade_documents", grade_documents)
+workflow.add_node("generate", generate)
+workflow.add_node("websearch", web_search)
+
+# Build graph
+workflow.set_entry_point("retrieve")
+workflow.add_edge("retrieve", "grade_documents")
+workflow.add_conditional_edges(
+    "grade_documents",
+    decide_to_generate,
+    {
+        "websearch": "websearch",
+        "generate": "generate",
+    },
+)
+workflow.add_edge("websearch", "generate")
+workflow.add_edge("generate", END)
+
+app = workflow.compile()
+
+if prompt := st.chat_input("Ask a question"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.write_stream(stream_data(prompt))
+
+    with st.chat_message("assistant"):
+        inputs = {"question": prompt}
+        output = app.invoke(inputs)
+
+        st.write(stream_data(output["generation"]))
+        st.session_state.messages.append({"role": "assistant", "content": output["generation"]})
+
+
+
+
+
+
+
+
+
+
+
+
+
--- rag_utils.py
+++ rag_utils.py
@@ -1,151 +1,138 @@
-import time
 import streamlit as st
+import time
+import pandas as pd
 
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.vectorstores import FAISS
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_openai import OpenAIEmbeddings
-
-from langchain_experimental.text_splitter import SemanticChunker 
-from langchain.retrievers import ContextualCompressionRetriever, EnsembleRetriever
+from PyPDF2 import PdfReader
 from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers import (
+    ContextualCompressionRetriever,
+    EnsembleRetriever,
+)
+from langchain_experimental.text_splitter import SemanticChunker
 from langchain_cohere import CohereRerank
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.prompts import ChatPromptTemplate
+
+from dotenv import load_dotenv
+load_dotenv()
 
 
-
-def configure_hybrid_search(data):
+def retrieve_documents(uploaded_file, file_format):
     """
-    Configures a hybrid search mechanism that combines semantic and keyword-based retrieval,
-    followed by reranking for optimized search results.
-    
-    Args:
-        data: The input data that will be chunked and used for retrieval.
+       This function handles the processing of an uploaded file (PDF, TXT, or CSV)
+       and creates a document retriever capable of retrieving context-relevant
+       documents for downstream tasks.
 
-    Returns:
-        compression_retriever: A retrieval system that combines hybrid search and reranking for improved search performance.
-    """
-    
+       Args:
+           uploaded_file: The file uploaded by the user, which can be in PDF, TXT, or CSV format.
+           file_format: A string indicating the format of the uploaded file, allowing the function to handle
+           the file appropriately.
+
+       Returns:
+           retriever: combines both similarity-based and keyword-based retrieval methods, enhanced by contextual
+           compression using a reranking model.
+       """
+
+    try:
+        if not uploaded_file:
+            welcome_message()
+            st.stop()
+
+        elif uploaded_file.size == 0:
+            st.info("The uploaded file is empty. Please upload a valid document.")
+            st.stop()
+
+        elif uploaded_file is not None:
+            if file_format == "pdf":
+
+                pdf_reader = PdfReader(uploaded_file)
+                data = "".join(page.extract_text() for page in pdf_reader.pages)
+
+            elif file_format == "txt":
+                data = uploaded_file.read().decode("utf-8")
+
+            elif file_format == "csv":
+                data = pd.read_csv(uploaded_file)
+                data = data.to_string(index=False)
+
+            else:
+                st.warning("Unsupported file format. Please upload a valid document (PDF, TXT, or CSV).")
+
+        else:
+            pass
+
+    except Exception as e:
+        st.info(f"An error occurred the upload: {str(e)}")
+
     semantic_chunker = SemanticChunker(
-        OpenAIEmbeddings(), 
+        OpenAIEmbeddings(),
         breakpoint_threshold_type="percentile",
-        )
-    
+    )
+
     docs = semantic_chunker.create_documents([data])
     vectorstore = FAISS.from_documents(docs, OpenAIEmbeddings())
-    
+
     similarity_retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
     keyword_retriever = BM25Retriever.from_documents(docs)
     keyword_retriever.k = 5
-    ensemble_retriever = EnsembleRetriever(retrievers=[similarity_retriever, keyword_retriever], 
+    ensemble_retriever = EnsembleRetriever(retrievers=[similarity_retriever, keyword_retriever],
                                            weights=[0.5, 0.5],
                                            )
-    
+
     compressor = CohereRerank(model="rerank-english-v3.0")
-    
-    compression_retriever = ContextualCompressionRetriever(
-    base_compressor=compressor, 
-    base_retriever=ensemble_retriever,
+
+    retriever = ContextualCompressionRetriever(
+        base_compressor=compressor,
+        base_retriever=ensemble_retriever,
     )
-    
-    return compression_retriever
+
+    return retriever
 
 
-
-def configure_rag_chain(retriever, llm):
+def create_rag_chain(retriever, llm):
     """
-    Configures a Retrieval-Augmented Generation (RAG) chain that uses a retriever and a large language model
-    to handle conversational question answering with session-specific history.
+     Creates a Retrieval-Augmented Generation (RAG) chain for answering questions by retrieving relevant
+     context from a retriever and generating concise responses with a language model.
 
-    Args:
-        retriever: The document retriever responsible for fetching relevant context from a knowledge base.
-        llm: The large language model used to generate answers to the user's questions.
+     Args:
+         retriever: The document retriever used to fetch relevant context.
+         llm: The language model responsible for generating responses.
 
-    Returns:
-        conversational_rag_chain: A RAG chain that handles input messages, retrieves relevant context,
-        manages chat history, and generates responses based on both the retrieved context and prior conversations.
-    """
-    
-    contextualize_q_system_prompt = (
-    "Given a chat history and the latest user question "
-    "which might reference context in the chat history, "
-    "formulate a standalone question which can be understood "
-    "without the chat history. Do NOT answer the question, "
-    "just reformulate it if needed and otherwise return it as is."
-    )
-    
-    contextualize_prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", contextualize_q_system_prompt),
-            MessagesPlaceholder("chat_history"),
-            ("human", "{input}"),
-        ]
-    )
-        
-    history_aware_retriever = create_history_aware_retriever(
-        llm, 
-        retriever, 
-        contextualize_prompt,
+     Returns:
+         rag_chain: A RAG chain that integrates document retrieval with question answering.
+     """
+
+    qa_system = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        "{context}"
     )
 
-    system_prompt = (
-    "You are an assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer "
-    "the question. If you don't know the answer, say that you "
-    "don't know. Use three sentences maximum and keep the "
-    "answer concise."
-    "\n\n"
-    "{context}"
-    )
-        
     qa_prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", system_prompt),
-            MessagesPlaceholder("chat_history"),
+            ("system", qa_system),
             ("human", "{input}"),
         ]
     )
 
     question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-    
-    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
-    store = {}
-    
-    def get_session_history(session_id: str) -> BaseChatMessageHistory:
-        if session_id not in store:
-            store[session_id] = ChatMessageHistory()
-        return store[session_id]
-
-    conversational_rag_chain = RunnableWithMessageHistory(
-        rag_chain,
-        get_session_history,
-        input_messages_key="input",
-        history_messages_key="chat_history",
-        output_messages_key="answer",
-    )
-
-    return conversational_rag_chain
-
-
-
-def stream_data(response):
-    
-    for word in response.split(" "):
-        yield word + " "
-        time.sleep(0.06)    
-
+    return rag_chain
 
 
 def welcome_message():
-    
     """
-    Displays a welcome message to the user if there are no previous messages in the session state. 
-    This function introduces the assistant and prompts the user to enter an API key and upload a file. 
-    The message is only shown once at the start of a new session.
+    Displays a welcome message to the user if there are no previous messages in the session state.This function
+    introduces the assistant and prompts the user to enter an API key and upload a file.The message is only shown
+    once at the start of a new session.
 
     Args:
         None
@@ -153,17 +140,21 @@ def welcome_message():
     Returns:
         None
     """
-    
+
     if not st.session_state.messages:
-        
-        welcome_message = """
-        
+        welcome_input = """
+
         Hello there! First, please specify the format of the file you'd like to upload. 
         Once you've uploaded your file, feel free to ask any questions about its content, and I'll provide 
         you with the relevant information. 👋
-        
+
         """
-        st.chat_message("assistant").write_stream(stream_data(welcome_message))
-        st.session_state.messages.append({"role": "assistant", "content": welcome_message})
-        
-         
+        st.chat_message("assistant").write_stream(stream_data(welcome_input))
+        st.session_state.messages.append({"role": "assistant", "content": welcome_input})
+
+
+def stream_data(response):
+    for word in response.split(" "):
+        yield word + " "
+        time.sleep(0.03)
+
